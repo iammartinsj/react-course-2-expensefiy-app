dce984b7e86375a7acf2521eff346a8e9e988400
Steve Martin <steve.sanjose@redcoresolutions.com>
2018-10-28 12:49:26 +0800

    remove dummy date in app.js file

--- src/app.js
+++ src/app.js
@@ -11,18 +11,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-//Add Expenses
-store.dispatch(addExpense({ description:'Gas bill', amount:1500, createdAt:0}));
-store.dispatch(addExpense({ description:'Water bill', amount:1000, createdAt:5000}));
-const expense3 = store.dispatch(addExpense({ description:'Internet bill', amount:2000, createdAt:0}));
-
-//Set Filters
-// store.dispatch(setFilterText('Bill'));
-
-//Display Visible Expenses
-
-// const state = store.getState();
-// console.log('visible',getVisibleExpenses(state.expenses, state.filter));
 
 const jsx = (
   <div>
